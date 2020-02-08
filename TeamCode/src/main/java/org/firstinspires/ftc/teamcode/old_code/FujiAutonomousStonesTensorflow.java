418f72b121a6460cd571225db687f4b5835be4b8
Yash <yash.gupta.7782@gmail.com>
2020-02-08 18:05:30 -0500

    Finishing touches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/old_code/FujiAutonomousStonesTensorflow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/old_code/FujiAutonomousStonesTensorflow.java
@@ -37,60 +37,60 @@ public class FujiAutonomousStonesTensorflow extends FujiAutonomous {
         }
 
 
-        while (!isStarted()) {
-            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-            List<RecognitionPair> objects = new ArrayList<RecognitionPair>();
-
-            // CHANGED JAVA VERSION AND API VERSION, EXPERIMENTAL CODE
-            if (updatedRecognitions != null) {
-
-                telemetry.addData("# Object Detected", updatedRecognitions.size());
-
-                // step through the list of recognitions and display boundary info.
-                int i = 0;
-                for (Recognition recognition : updatedRecognitions) {
-                    //telemetry updates
-
-                    String label = recognition.getLabel();
-                    Float coordinate = (recognition.getLeft() + recognition.getRight()) / 2;
-
-                    telemetry.addData("confidence: ", recognition.getConfidence());
-                    telemetry.addData(String.format("label (%d)", i), label);
-                    telemetry.addData(String.format("center x coordinate (%d)", i), "%.03f",
-                            coordinate);
-
-                    //add to list
-                    objects.add(new RecognitionPair(label, coordinate));
-
-
-                    i += 1;
-                }
-                telemetry.update();
-
-                // really terrible code just to sort on x-coordinate
-                Collections.sort(objects, new Comparator<RecognitionPair>() {
-                    @Override
-                    public int compare(RecognitionPair r1, RecognitionPair r2) {
-                        return r1.xcoordinate.compareTo(r2.xcoordinate);
-                    }
-                });
-
-                ArrayList<String> labels = new ArrayList<String>();
-
-                objects.forEach((recog) -> labels.add(recog.label));
-
-
-                // SKYSTONE IS SET HERE
-                if (labels.contains("Skystone")) {
-                    skystone = labels.indexOf("Skystone");
-                } else {
-                    skystone = 3;
-                }
-
-
-
-            }
-        }
+//        while (!isStarted()) {
+//            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+//            List<RecognitionPair> objects = new ArrayList<RecognitionPair>();
+//
+//            // CHANGED JAVA VERSION AND API VERSION, EXPERIMENTAL CODE
+//            if (updatedRecognitions != null) {
+//
+//                telemetry.addData("# Object Detected", updatedRecognitions.size());
+//
+//                // step through the list of recognitions and display boundary info.
+//                int i = 0;
+//                for (Recognition recognition : updatedRecognitions) {
+//                    //telemetry updates
+//
+//                    String label = recognition.getLabel();
+//                    Float coordinate = (recognition.getLeft() + recognition.getRight()) / 2;
+//
+//                    telemetry.addData("confidence: ", recognition.getConfidence());
+//                    telemetry.addData(String.format("label (%d)", i), label);
+//                    telemetry.addData(String.format("center x coordinate (%d)", i), "%.03f",
+//                            coordinate);
+//
+//                    //add to list
+//                    objects.add(new RecognitionPair(label, coordinate));
+//
+//
+//                    i += 1;
+//                }
+//                telemetry.update();
+//
+//                // really terrible code just to sort on x-coordinate
+//                Collections.sort(objects, new Comparator<RecognitionPair>() {
+//                    @Override
+//                    public int compare(RecognitionPair r1, RecognitionPair r2) {
+//                        return r1.xcoordinate.compareTo(r2.xcoordinate);
+//                    }
+//                });
+//
+//                ArrayList<String> labels = new ArrayList<String>();
+//
+//                objects.forEach((recog) -> labels.add(recog.label));
+//
+//
+//                // SKYSTONE IS SET HERE
+//                if (labels.contains("Skystone")) {
+//                    skystone = labels.indexOf("Skystone");
+//                } else {
+//                    skystone = 3;
+//                }
+//
+//
+//
+//            }
+//        }
         //once the opmode starts, we should know what stone is the skystone, so we can immediate race off to it
         //
     }
